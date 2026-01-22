d3fc03497530a1393317ffa8ba45b077209952b6
Nasoooor <Nasoooor29@gmail.com>
2026-01-22 09:44:13 +0300

    simplify types

--- src/App.tsx
+++ src/App.tsx
@@ -12,18 +12,10 @@ import {
     Filler
 } from 'chart.js';
 
-export type FanControlMode = 'percentage' | 'rpm';
 
 export interface FanCurvePoint {
     temperature: number; // 0-100
     fanSpeed: number; // percentage (0-100) or RPM value
-}
-
-export interface FanCurve {
-    id: string;
-    name: string;
-    mode: FanControlMode;
-    points: FanCurvePoint[];
 }
 
 ChartJS.register(
